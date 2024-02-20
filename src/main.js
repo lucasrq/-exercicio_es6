e6429915d8cd0179df831936b42233faf45bcb38
lucasrq <lucasroque545@gmail.com>
2024-02-20 18:21:30 -0300

    new

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const alunos = [
+    { nome: 'Lucas', nota1: 5, nota2: 8 },
+    { nome: 'Anne', nota1: 9, nota2: 7 },
+    { nome: 'Igor', nota1: 2, nota2: 4 },
+    { nome: 'Charles', nota1: 1, nota2: 2 }
+];
+
+const alunosAprovados = alunos.filter(aluno => (aluno.nota1 + aluno.nota2) / 2 >= 6);
+
+console.log(alunosAprovados);
